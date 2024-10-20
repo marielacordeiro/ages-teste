7be3f81f2c53c2cd05d704011ef0d2fb512ff690
marielacordeiro <marielapontes@icloud.com>
2024-10-20 18:35:50 -0300

    modificando plmds

--- programa.py
+++ programa.py
@@ -1,9 +1,12 @@
-def par_ou_impar(numero):
-    if numero % 2 == 0:
-        return f"{numero} é par."
+def soma_numeros(n):
+    return sum(range(1, n + 1))
+
+try:
+    numero = int(input("Digite um número inteiro positivo: "))
+    if numero < 1:
+        print("Por favor, digite um número positivo.")
     else:
-        return f"{numero} é ímpar."
-
-numero = int(input("Digite um número: "))
-
-print(par_ou_impar(numero))
+        resultado = soma_numeros(numero)
+        print(f"A soma dos números de 1 até {numero} é: {resultado}")
+except ValueError:
+    print("Por favor, digite um número inteiro válido.")
